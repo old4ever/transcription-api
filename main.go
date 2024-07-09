20cc1fb683b6d215d441e311cc244bcb1d5523a9
Dmytro Stanchiev <git@old4ever.simplelogin.com>
2024-07-09 15:20:24 -0400

    fix: added cors

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/joho/godotenv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"context"
@@ -43,6 +44,18 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		// AllowMethods:     []string{"POST", "PATCH"},
+		// AllowHeaders:     []string{"Origin"},
+		// ExposeHeaders:    []string{"Content-Length"},
+		// AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		// MaxAge: 12 * time.Hour,
+	}))
 
 	r.POST("/audio/start", startRecording)
 	r.POST("/audio/stop", stopRecording)
